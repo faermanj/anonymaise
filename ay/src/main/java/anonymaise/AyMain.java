b0c48f647ab0382fdca13185c809f0397daecea0
Julio Faerman <356476+faermanj@users.noreply.github.com>
2025-10-06 22:08:40 +0200

    feat: Remove legacy Dockerfiles and update AyMain class for Quarkus application

--- ay/src/main/java/anonymaise/AyMain.java
+++ ay/src/main/java/anonymaise/AyMain.java
@@ -1,21 +1,28 @@
 package anonymaise;
 
+import io.quarkus.logging.Log;
+import io.quarkus.picocli.runtime.PicocliRunner;
 import io.quarkus.picocli.runtime.annotations.TopCommand;
-import picocli.CommandLine.Command;
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import jakarta.inject.Inject;
+import picocli.CommandLine;
 
+@QuarkusMain
 @TopCommand
-@Command(name = "ay", mixinStandardHelpOptions = true, description = "Database Anonymaiser")
-public class AyMain implements Runnable {
+@CommandLine.Command(name = "ay", mixinStandardHelpOptions = true)
+public class AyMain implements QuarkusApplication, Runnable {
+    @Inject
+    CommandLine.IFactory factory; 
 
     @Override
     public void run() {
-        // No-op: show help/usage or run a default action if needed
-        System.out.println("AY AY AY! Anonyimaise WILL destroy your data, last chance to quit and BACKUP!");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt(); // Restore interrupted status
-        }
-        System.out.println("ay ay ay.");
+        Log.info("Ay...");
+    }
+
+    @Override
+    public int run(String... args) throws Exception {
+        return new CommandLine(this, factory).execute(args);
     }
 }
